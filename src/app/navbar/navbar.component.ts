3066f674d8edf799eaf68235cc8db9b1af7431d4
Jasper Alakoskela <jasper.alakoskela@gmail.com>
2022-01-20 13:09:33 +0200

    Etusivujen tekoa

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -25,17 +25,11 @@ export class NavbarComponent implements OnInit {
       showOnTablet: true,
       showOnDesktop: true
     },
-    
+
     {
-      label: 'Yhteistiedot',
+      label: 'Yhteystiedot',
       showOnMobile: false,
       showOnTablet: true,
-      showOnDesktop: true
-    },
-    {
-      label: 'Yhteydenotto',
-      showOnMobile: false,
-      showOnTablet: false,
       showOnDesktop: true
     },
     {
@@ -58,30 +52,30 @@ export class NavbarComponent implements OnInit {
 
   scroll = (): void => {
 
-   let scrollHeigth;
+    let scrollHeigth;
 
-   if(window.innerWidth < 150){
-    scrollHeigth = 10;
-   }else if(window.innerWidth < 300 && window.innerWidth > 150){
-    scrollHeigth = 110;
-   }else if(window.innerWidth < 400 && window.innerWidth > 300){
-    scrollHeigth = 210;
-   }else if(window.innerWidth < 700 && window.innerWidth > 400){
-    scrollHeigth = 310;
-   }else{
-     scrollHeigth = 410;
-   }
+    if (window.innerWidth < 150) {
+      scrollHeigth = 10;
+    } else if (window.innerWidth < 300 && window.innerWidth > 150) {
+      scrollHeigth = 110;
+    } else if (window.innerWidth < 400 && window.innerWidth > 300) {
+      scrollHeigth = 210;
+    } else if (window.innerWidth < 700 && window.innerWidth > 400) {
+      scrollHeigth = 310;
+    } else {
+      scrollHeigth = 1;
+    }
 
-    if(window.scrollY >= scrollHeigth){
-      document.body.style.setProperty('--navbar-scroll', "rgb(243,127,41,255)");
-      document.body.style.setProperty('--navbar-scroll-text', " color: rgb(85, 45, 13)");
-      document.body.style.setProperty('--navbar-scroll-shadow', "0px 6px 12px -5px #000000");
-    
-    }else if(window.scrollY < scrollHeigth){
-      document.body.style.setProperty('--navbar-scroll', "transparent");
+    if (window.scrollY >= scrollHeigth) {
+      document.body.style.setProperty('--navbar-scroll', "#3C424C");
       document.body.style.setProperty('--navbar-scroll-text', "white");
+      document.body.style.setProperty('--navbar-scroll-shadow', "0px 6px 12px -5px #000000");
+
+    } else if (window.scrollY < scrollHeigth) {
+      document.body.style.setProperty('--navbar-scroll', "transparent");
+      document.body.style.setProperty('--navbar-scroll-text', "#3C424C");
       document.body.style.setProperty('--navbar-scroll-shadow', "none");
- 
+
 
     }
   }
