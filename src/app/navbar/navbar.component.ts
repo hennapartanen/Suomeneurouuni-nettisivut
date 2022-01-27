8607fa22d0631aa85947f0e694d0c7a3e32c9b33
hennapartanen <57128825+hennapartanen@users.noreply.github.com>
2022-01-27 14:06:00 +0200

    Update navbar.component.ts

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,7 +1,9 @@
-import { ScrollDispatcher } from '@angular/cdk/scrolling';
+
 import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'src/app/navbar/menu-item';
-import { NgZone } from '@angular/core';
+
+import { ViewportScroller } from '@angular/common';
+import { Router } from '@angular/router';
 
 
 
@@ -15,6 +17,7 @@ export class NavbarComponent implements OnInit {
   menuItems: MenuItem[] = [
     {
       label: 'Galleria',
+    
       showOnMobile: true,
       showOnTablet: true,
       showOnDesktop: true
@@ -26,29 +29,66 @@ export class NavbarComponent implements OnInit {
       showOnDesktop: true
     },
 
-    {
-      label: 'Yhteystiedot',
-      showOnMobile: false,
-      showOnTablet: true,
-      showOnDesktop: true
-    },
+  
     {
       label: 'Palvelut',
+    
       showOnMobile: false,
       showOnTablet: false,
       showOnDesktop: false
     },
     {
+      label: 'Yhteystiedot',
+  
+      showOnMobile: false,
+      showOnTablet: true,
+      showOnDesktop: true
+    },
+    {
       label: 'Verkkokauppa',
+
       showOnMobile: false,
       showOnTablet: false,
       showOnDesktop: false
     },
   ];
 
+  constructor(private scroller: ViewportScroller, private router: Router) {}
   ngOnInit(): void {
-    window.addEventListener('scroll', this.scroll, true)
+    window.addEventListener('scroll', this.scroll, true);
+
   }
+
+  goDown1() {
+    this.scroller.scrollToAnchor("targetGalleria");
+  }
+
+  goDown2() {
+    //this.scroller.scrollToAnchor("targetGreen");
+    document.getElementById("targetPalvelut").scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "nearest"
+    });
+  }
+
+  goDown3() {
+    //this.scroller.scrollToAnchor("targetGreen");
+    document.getElementById("targetYhteistiedot").scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "nearest"
+    });
+  }
+  goDown4() {
+    //this.scroller.scrollToAnchor("targetGreen");
+    document.getElementById("targetVerkkokauppa").scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "nearest"
+    });
+  }
+ 
 
   scroll = (): void => {
 
@@ -63,17 +103,17 @@ export class NavbarComponent implements OnInit {
     } else if (window.innerWidth < 700 && window.innerWidth > 400) {
       scrollHeigth = 310;
     } else {
-      scrollHeigth = 1;
+      scrollHeigth = 2;
     }
 
     if (window.scrollY >= scrollHeigth) {
-      document.body.style.setProperty('--navbar-scroll', "#3C424C");
-      document.body.style.setProperty('--navbar-scroll-text', "white");
+      document.body.style.setProperty('--navbar-scroll', "#FC842B");
+      document.body.style.setProperty('--navbar-scroll-text', "#3C424C");
       document.body.style.setProperty('--navbar-scroll-shadow', "0px 6px 12px -5px #000000");
 
-    } else if (window.scrollY < scrollHeigth) {
+    } else if (window.scrollY <= scrollHeigth) {
       document.body.style.setProperty('--navbar-scroll', "transparent");
-      document.body.style.setProperty('--navbar-scroll-text', "#3C424C");
+      document.body.style.setProperty('--navbar-scroll-text', "white");
       document.body.style.setProperty('--navbar-scroll-shadow', "none");
 
 
