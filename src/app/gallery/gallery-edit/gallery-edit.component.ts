817fedd42e52b0e198f177966ad654bf78fb718b
hennapartanen <57128825+hennapartanen@users.noreply.github.com>
2022-03-15 11:35:26 +0200

    Update gallery-edit.component.ts

--- src/app/gallery/gallery-edit/gallery-edit.component.ts
+++ src/app/gallery/gallery-edit/gallery-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core'; 
 import { ActivatedRoute, Params, Router } from '@angular/router'; 
-import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms'; 
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms'; 
 import { GalleryService } from '../gallery.service'; 
 
 
@@ -18,8 +18,6 @@ export class GalleryEditComponent implements OnInit {
   editMode = false; 
   itemForm: FormGroup; 
 
- 
-
   constructor(private route: ActivatedRoute, 
               private galleryService: GalleryService, 
               private router: Router) { 
@@ -28,6 +26,7 @@ export class GalleryEditComponent implements OnInit {
 
  
   ngOnInit() { 
+
     this.route.params 
       .subscribe( 
         (params: Params) => { 
@@ -37,7 +36,9 @@ export class GalleryEditComponent implements OnInit {
 
         } 
       ); 
+      
   } 
+
 
   onSubmit() { 
 
@@ -47,49 +48,56 @@ export class GalleryEditComponent implements OnInit {
       this.galleryService.addItem(this.itemForm.value); 
     } 
     this.onCancel(); 
-  } 
 
+  } 
+  
+  onAddTuote() {
+    (<FormArray>this.itemForm.get('tuotteet')).push(
+      new FormGroup({
+        name: new FormControl(null, Validators.required),
+        imagePath: new FormControl(null, Validators.required),
+        description: new FormControl(null, Validators.required),
+      })
+    );
+  }
+
+  onDeleteTuote(index: number) {
+    (<FormArray>this.itemForm.get('tuotteet')).removeAt(index);
+  }
+  
 
   onCancel() { 
     this.router.navigate(['../'], {relativeTo: this.route}); 
   } 
-
-
 
   private initForm() { 
     let itemName = ''; 
     let itemImagePath = ''; 
     let itemTuotteet = new FormArray([]);
 
-    if (this.editMode) { 
-      const item = this.galleryService.getItem(this.id); 
-      itemName = item.name; 
-      itemImagePath = item.imagePath; 
+    if (this.editMode) {
+      const item = this.galleryService.getItem(this.id);
+      itemName = item.name;
+      itemImagePath = item.imagePath;
       if (item['tuotteet']) {
         for (let tuote of item.tuotteet) {
           itemTuotteet.push(
             new FormGroup({
               name: new FormControl(tuote.name, Validators.required),
-              imagepath: new FormControl(tuote.imagePath, Validators.required),
+              imagePath: new FormControl(tuote.imagePath, Validators.required),
               description: new FormControl(tuote.description, Validators.required),
-            
+     
             })
           );
         }
       }
-     
-
-    } 
-
+    }
 
     this.itemForm = new FormGroup({ 
-      'name': new FormControl(itemName, Validators.required), 
-      'imagePath': new FormControl(itemImagePath, Validators.required), 
-     tuotteet: itemTuotteet
-
+      name: new FormControl(itemName, Validators.required), 
+      imagePath: new FormControl(itemImagePath, Validators.required), 
+      tuotteet: itemTuotteet
     }); 
-
   } 
 
- 
 } 
