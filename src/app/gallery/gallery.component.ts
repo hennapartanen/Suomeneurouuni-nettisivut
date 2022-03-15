e83ff0bafd3e5742e74ac11c80952056a59efb19
hennapartanen <57128825+hennapartanen@users.noreply.github.com>
2022-03-15 11:36:57 +0200

    Update gallery.component.ts

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,17 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { Item } from './gallery.model';
+import { Component, OnInit, OnDestroy } from '@angular/core'; 
+import { Subscription } from 'rxjs';
+import { Item } from './gallery.model'; 
 
-@Component({
-  selector: 'app-gallery',
-  templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.css']
-})
-export class GalleryComponent implements OnInit {
-  selectedItem: Item;
-  
-  constructor() { }
+import { AuthService } from '../auth/auth.service';
 
-  ngOnInit(): void {
+@Component({ 
+
+  selector: 'app-gallery', 
+  templateUrl: './gallery.component.html', 
+  styleUrls: ['./gallery.component.css'] 
+
+}) 
+
+export class GalleryComponent implements OnInit { 
+  selectedItem: Item; 
+  isAuthenticated = false;
+  private userSub: Subscription;
+
+
+  constructor(private authService: AuthService) { 
+  } 
+
+  ngOnInit() {
+    this.userSub = this.authService.user.subscribe(user => {
+      this.isAuthenticated = !!user;
+      console.log(!user);
+      console.log(!!user);
+    });
   }
+  
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
+  }
+} 
 
-}
+ 
+ 
