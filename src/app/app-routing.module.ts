b6dcc2180d5544bd95f6eb59a46284158daa9867
hennapartanen <57128825+hennapartanen@users.noreply.github.com>
2022-03-15 11:39:20 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,30 +4,33 @@ import { GalleryComponent } from './gallery/gallery.component';
 import { GalleryStartComponent } from './gallery/gallery-start/gallery-start.component'; 
 import { GalleryEditComponent } from './gallery/gallery-edit/gallery-edit.component'; 
 import { GalleryDetailComponent } from './gallery/gallery-detail/gallery-detail.component'; 
-import { GalleryItemitComponent } from './gallery-itemit/gallery-itemit.component';
-import { GalleryItemitStartComponent } from './gallery-itemit/gallery-itemit-start/gallery-itemit-start.component';
-import { GalleryItemitEditComponent } from './gallery-itemit/gallery-itemit-edit/gallery-itemit-edit.component';
-import { GalleryItemitDetailComponent } from './gallery-itemit/gallery-itemit-detail/gallery-itemit.detail.component';
-import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './auth/auth.guard';
+ import { AuthComponent } from './auth/auth.component';
+ import { GalleryResolverService } from './gallery/gallery-resolver.service';
+
 
 const appRoutes: Routes = [ 
 
-  { path: '', redirectTo: '/homepage', pathMatch: 'full' }, 
-  { path: 'auth', component: AuthComponent }, 
-  { path: 'gallery', component: GalleryComponent, children: [ 
-    { path: '', component: GalleryStartComponent }, 
-    { path: 'new', component: GalleryEditComponent }, 
-    { path: ':id', component: GalleryDetailComponent }, 
-    { path: ':id/edit', component: GalleryEditComponent }, 
-    { path: ':id/category', component: GalleryItemitComponent },
-    { path: ':id/category/new', component: GalleryItemitEditComponent },
-    { path: ':id/category/:id1', component: GalleryItemitDetailComponent },
-    { path: ':id/category/:id1/edit', component: GalleryItemitEditComponent },
-  ] }, 
+  { path: '', redirectTo: '', pathMatch: 'full' },
+  {
+    path: 'gallery', component: GalleryComponent,
+  
+    children: [
+      { path: '', component: GalleryStartComponent, },
+      { path: 'new', component: GalleryEditComponent, canActivate: [AuthGuard],},
+      { path: ':id', component: GalleryDetailComponent, canActivate: [AuthGuard],
+        resolve: [GalleryResolverService]
+      },
+      {
+        path: ':id/edit', component: GalleryEditComponent,  canActivate: [AuthGuard],
+        resolve: [GalleryResolverService]
+      }
+    ]
+  },
+  { path: 'auth', component: AuthComponent }
+];
 
-
-
- ]; 
+ 
 
  
  
