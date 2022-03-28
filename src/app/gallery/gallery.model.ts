1d753c5c4c2358a57cb12d12eb717da25203e7d4
hennapartanen <57128825+hennapartanen@users.noreply.github.com>
2022-03-28 09:19:09 +0300

    Update gallery.model.ts

--- src/app/gallery/gallery.model.ts
+++ src/app/gallery/gallery.model.ts
@@ -1,15 +1,17 @@
 import { Tuote } from "../shared/tuote.model";
 
-export class Item {
-    public name: string;
-    public imagePath: string;
-    public tuotteet: Tuote[];
-  
-  
-    constructor(name: string, imagePath: string, tuotteet: Tuote[]) {
+export class Item { 
+    name: string; 
+    imagePath: string;
+    tuotteet: Tuote[];
+
+    constructor( name: string, imagePath: string, tuotteet: Tuote[]) { 
       this.name = name;
-      this.imagePath = imagePath;
+      this.imagePath= imagePath;
       this.tuotteet = tuotteet;
-    }
-  }
-  
+   
+    } 
+
+  } 
+
+   
