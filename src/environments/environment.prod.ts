593fad955d9e7762cb517e5255e264e65b06244b
hennapartanen <57128825+hennapartanen@users.noreply.github.com>
2022-04-21 13:19:32 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,19 @@
-export const environment = {
-  production: true
-};
+ 
+
+export const environment = { 
+
+  production: true, 
+
+  firebaseConfig : { 
+    apiKey: "AIzaSyC-O0-Lj0tjPToBSQpFqIcgGJaMt8Ia6YA", 
+    authDomain: "eurouuni-ced0b.firebaseapp.com", 
+    databaseURL: "https://eurouuni-ced0b-default-rtdb.europe-west1.firebasedatabase.app", 
+    projectId: "eurouuni-ced0b", 
+    storageBucket: "eurouuni-ced0b.appspot.com", 
+    messagingSenderId: "656902143819", 
+    appId: "1:656902143819:web:b9849abebd514f1e30f2ce", 
+
+  } 
+
+}; 
+
